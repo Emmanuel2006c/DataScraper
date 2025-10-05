f7739fc976daeb4e8430e0fcc1acecbf19def67c
Emmanuel Maragakis <62173368+Emmanuel2006bot@users.noreply.github.com>
2025-10-05 23:44:51 +0200

    Erster Code

--- Scraper.py
+++ Scraper.py
@@ -1,1 +1,13 @@
-print("HElloWoerld")
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get("https://www.coingecko.com/de")
+
+soup = BeautifulSoup(url.text, 'html.parser')
+
+
+#f = open("coingecko.html", 'r', encoding='utf-8')
+#inhalt = f.read()
+#zeilen = f.readlines()
+#f.close()
+#print(zeilen[:10])
