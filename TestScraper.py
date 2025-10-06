a7fd8d60644df2fa8e01faa1c2e254d3fe1b7231
Porzl2k5 <leo.prentl@gmail.com>
2025-10-06 17:27:23 +0200

    Country:Capital

--- TestScraper.py
+++ TestScraper.py
@@ -1,9 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://www.scrapethissite.com/'
+url = 'https://www.scrapethissite.com/pages/simple/'
 page = requests.get(url)
 page_status = page.status_code
+html_file = BeautifulSoup(page.text, 'html.parser')
+header = html_file.find('title')
+j = 0
+
 print(page_status)
-html_file = BeautifulSoup(page.text, 'html')
-print(html_file)
+
+print(header)
+
+for h3 in html_file.find_all('h3'):
+    country = str(h3.get_text(strip=True))
+    i = 0
+    for span in html_file.find_all('span', class_='country-capital'):
+        capital = str(span.get_text(strip=True))
+        if i < j:
+            i = i + 1
+            continue
+        print(country + ': ' + capital)
+        j = j + 1
+        break
